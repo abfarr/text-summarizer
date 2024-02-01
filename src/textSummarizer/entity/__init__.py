d4d975b60935a8bc6d3ad91984340d4baf9f7e6a
abfarr <alexibell13@gmail.com>
2024-02-01 10:54:07 -0500

    data validation stage added

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -4,9 +4,19 @@ from pathlib import Path
 @dataclass(frozen=True)
 class DataIngestionConfig:
     '''
-    Controls entity
+    Defines types for ingestion valiables imported from text-summarizer/config/config.yaml
     '''
     root_dir: Path # variable from config.yaml
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    '''
+    Defines types for validation valiables imported from text-summarizer/config/config.yaml
+    '''
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -1,5 +1,5 @@
-from textSummarizer.config import ConfigurationManager
-from textSummarizer.components import DataIngestion
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.data_ingestion import DataIngestion
 from textSummarizer.logging import logger
 
 
